dcb300f02f02101c8fb09b827ce03b26514683ec
Shailendra Prakash <prakashshailendra45@gmail.com>
2025-06-21 23:13:35 +0530

    hi

--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -14,35 +14,37 @@ public class TC001_AccountRegistrationTest extends BaseClass {
 	public void verify_account_registration() {
 		logger.info("*********Test Starting TC001_AccountRegistrationTest*************");
 		try {
-		HomePage hp = new HomePage(driver);
-		hp.clickMyAccount();
-		logger.info("Clicked on my Account");
-		hp.clickRegister();
-		logger.info("Clicked on Register");
+			HomePage hp = new HomePage(driver);
+			Thread.sleep(1000); // Wait for page to load
+			hp.clickMyAccount();
+			logger.info("Clicked on my Account");
+			Thread.sleep(500); // Wait for dropdown to appear
+			hp.clickRegister();
+			logger.info("Clicked on Register");
 
-		AccountRegistrationPage regpage = new AccountRegistrationPage(driver);
-		logger.info("Providing Customer details...");
-		regpage.setFirstName(randomString().toUpperCase());
-		regpage.setLastName(randomString().toUpperCase());
-		regpage.setEmail(randomString()+"@gmail.com");
-		regpage.setTelephone(randomNumbers());
-		
-		String password=randomAlphaNumeric();
-		regpage.setPassword(password);
-		regpage.setConfirmPassword(password);
-		
-		regpage.setPrivacyPolicy();
-		regpage.clickContinue();
-		
-		String confMsg = regpage.getConfirmationMsg();
-		Assert.assertEquals(confMsg,"Your Account Has Been Created!");
-		}catch(Exception e) {
-			logger.error("Test Failed...");
-			logger.debug("Debug Logs");
-			Assert.fail("Test failed: " + e.getMessage());
-		}
-		logger.info("***************Test Completed**********************");
-		
+			AccountRegistrationPage regpage = new AccountRegistrationPage(driver);
+			logger.info("Providing Customer details...");
+			regpage.setFirstName(randomString().toUpperCase());
+			regpage.setLastName(randomString().toUpperCase());
+			regpage.setEmail(randomString()+"@gmail.com");
+			regpage.setTelephone(randomNumbers());
+
+			String password=randomAlphaNumeric();
+			regpage.setPassword(password);
+			regpage.setConfirmPassword(password);
+
+			regpage.setPrivacyPolicy();
+			regpage.clickContinue();
+
+			String confMsg = regpage.getConfirmationMsg();
+			Assert.assertEquals(confMsg,"Your Account Has Been Created!");
+			}catch(Exception e) {
+				logger.error("Test Failed...");
+				logger.debug("Debug Logs");
+				Assert.fail("Test failed: " + e.getMessage());
+			}
+			logger.info("***************Test Completed**********************");
+
 
 	}
 	
